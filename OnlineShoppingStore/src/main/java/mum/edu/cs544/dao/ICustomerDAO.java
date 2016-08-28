ea027d1439ba42fe16b3802e1c56588c121e52e7
tarekegnsisay <tarea2003@gmail.com>
2016-08-28 17:18:57 -0500

    Service and DAO

--- OnlineShoppingStore/src/main/java/mum/edu/cs544/dao/ICustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package mum.edu.cs544.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mum.edu.cs544.models.Customer;
-
-public interface ICustomerDAO extends JpaRepository<Customer,Long>{
-
-}
